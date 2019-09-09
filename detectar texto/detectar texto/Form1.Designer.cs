ccba31c458c77d5619cb7cde580042727b744ccf
emanuelmoreno33 <emanuel.moreno16@tectijuana.edu.mx>
2019-09-08 17:07:35 -0700

    primera version, lee 2 numeros, un signo de operacion basica y ocupa es igual a

---
+++ detectar texto/detectar texto/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace detectar_texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txttexto = new System.Windows.Forms.TextBox();
+            this.btndetectar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txttexto
+            // 
+            this.txttexto.Location = new System.Drawing.Point(12, 41);
+            this.txttexto.Name = "txttexto";
+            this.txttexto.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txttexto.Size = new System.Drawing.Size(395, 22);
+            this.txttexto.TabIndex = 0;
+            // 
+            // btndetectar
+            // 
+            this.btndetectar.Location = new System.Drawing.Point(559, 26);
+            this.btndetectar.Name = "btndetectar";
+            this.btndetectar.Size = new System.Drawing.Size(98, 52);
+            this.btndetectar.TabIndex = 2;
+            this.btndetectar.Text = "Detectar";
+            this.btndetectar.UseVisualStyleBackColor = true;
+            this.btndetectar.Click += new System.EventHandler(this.btndetectar_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(559, 84);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(98, 46);
+            this.btnlimpiar.TabIndex = 6;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "    ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "uno",
+            "dos",
+            "tres",
+            "cuatro",
+            "cinco",
+            "seis",
+            "siete",
+            "ocho",
+            "nuevo",
+            "cero"});
+            this.listBox1.Location = new System.Drawing.Point(12, 180);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 180);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Numeros";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Operaciones";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "sumar a",
+            "restar a",
+            "multiplicar con",
+            "dividir entre",
+            "es igual a"});
+            this.listBox2.Location = new System.Drawing.Point(190, 180);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 180);
+            this.listBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 407);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btndetectar);
+            this.Controls.Add(this.txttexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txttexto;
+        private System.Windows.Forms.Button btndetectar;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ detectar texto/detectar texto/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace detectar_texto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndetectar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            string texto = txttexto.Text;
+            var palabras = texto.Split((string[])null,StringSplitOptions.RemoveEmptyEntries);
+            int totalpalabras = palabras.Count();
+            
+                if (totalpalabras == 7)
+                {
+                foreach (var palabra in palabras)
+                {
+                    verificar(palabra);
+                }
+                }
+                else if (totalpalabras < 7)
+                {
+                    MessageBox.Show("Faltan palabras");
+                }
+                else
+                {
+                    MessageBox.Show("Hay mas palabras de las debidas");
+                }
+            
+        }
+
+        int? numero1 = null;
+        int? numero2 = null;
+        int? total = null;
+        int contador = 0;
+        string operacion = "";
+        string comprobacion = "";
+
+        string[] numeros = new string[] { "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" };
+        string[] operaciones = new string[] { "sumara", "restara", "multiplicarcon", "dividirentre" };
+
+        private void verificar(string palabra)
+        {
+            contador++;
+
+            if (contador == 1 || contador == 4)
+            {
+                bool numeroencontrado = false;
+                int numero = 0;
+                foreach (string item in numeros)
+                {
+                    if (item == palabra)
+                    {
+                        if (numero1 == null)
+                        {
+                            numero1 = numero;
+                            numeroencontrado = true;
+                            break;
+                        }
+                        else if (numero2 == null)
+                        {
+                            numero2 = numero;
+                            numeroencontrado = true;
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("error asignacion de numero");
+                            break;
+                        }
+                    }
+                    numero++;
+                }
+                if (numeroencontrado == false)
+                {
+                    MessageBox.Show("Numero no valido:" + palabra);
+
+                }
+                bool operacionvalida = false;
+                if (contador == 4)
+                {
+                    if (numero1 != null && numero2 != null)
+                    {
+                        foreach (string item in operaciones)
+                        {
+                            if (operacion == item)
+                            {
+                                switch (operacion)
+                                {
+                                    case "sumara":
+                                        total = numero1 + numero2;
+                                        operacionvalida = true;
+                                        break;
+                                    case "restara":
+                                        total = numero2 - numero1;
+                                        operacionvalida = true;
+                                        break;
+                                    case "multiplicarcon":
+                                        total = numero1 * numero2;
+                                        operacionvalida = true;
+                                        break;
+                                    case "dividirentre":
+                                        total = numero1 / numero2;
+                                        operacionvalida = true;
+                                        break;
+                                }
+                            }
+                        }
+
+                    }
+                    if (operacionvalida == false)
+                    {
+                        MessageBox.Show("Operacion no valida " + operacion);
+                    }
+                }
+                
+            }
+
+            else if (contador > 1 && contador < 8)
+            {
+                if (contador == 2 || contador == 3)
+                {
+                    operacion = operacion + palabra;
+                }
+                
+                if (contador > 4 && contador < 8)
+                {
+                    comprobacion = comprobacion + palabra;
+                    if (contador == 7)
+                    {
+                        if (comprobacion == "esiguala")
+                        {
+                            label1.Text = total.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No hay fin de resultado");
+                        }
+                    }
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hay palabras no validas: " + palabra);
+            }
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            txttexto.Clear();
+            label1.Text = "";
+        }
+
+        private void limpiar()
+        {
+            operacion = "";
+            comprobacion = "";
+            numero1 = null;
+            numero2 = null;
+            total = null;
+            contador = 0;
+        }
+    }
+}
